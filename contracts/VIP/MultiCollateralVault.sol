37bff6b105df71c447b1abfe86c4a881455f4286
Shadow001017 <ryanniit0x902@gmail.com>
2024-02-06 17:34:37 -0500

    implement exchangerate

--- contracts/VIP/MultiCollateralVault.sol
+++ contracts/VIP/MultiCollateralVault.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Owned.sol";
 
 contract MultiCollateralVault is Ownable {
     // Data structure to hold collateral information
